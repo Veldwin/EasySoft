ec5bc1536a63cc534498dbda83355e1a6e2e7812
Veldwin <link334933@gmail.com>
2022-11-23 15:45:18 +0100

    création de l'environnement

---
+++ EasySoft/View/View.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySoft.View
+{
+    class View
+    {
+
+        /// <summary>
+        /// Show this panel when the console open
+        /// </summary>
+        public void ShowStart()
+        {
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("|||            Welcome to EasySave               |||");
+            Console.WriteLine("----------------------------------------------------");
+        }
+        public void ShowMenu()
+        {
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("|||                   Menu                       |||");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("|||  0. Exit                                     |||");
+            Console.WriteLine("|||  1. Open a backup job                        |||");
+            Console.WriteLine("|||  2. Create a backup job                      |||");
+            Console.WriteLine("----------------------------------------------------");
+            Console.Write("Please enter the menu number : ");
+        }
+    }
+}
