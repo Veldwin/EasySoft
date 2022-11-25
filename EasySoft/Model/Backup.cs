a2449b809b83c012f27bb9e84c476424d949f606
camille351 <95225227+camille351@users.noreply.github.com>
2022-11-25 13:59:04 +0100

    Backup.cs

---
+++ EasySoft/Model/Backup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySoft.Model
+{
+    class Backup
+    {
+
+        public string RessourceBackup { get; set; }
+        public string TargetBackup { get; set; }
+        public string SaveName { get; set; }
+        public int Type { get; set; }
+        public string MirrorBackup { get; set; }
+
+        public Backup(string saveName, string source, string target, int type, string mirror)
+        {
+            SaveName = saveName;
+            RessourceBackup = source;
+            TargetBackup = target;
+            Type = type;
+            MirrorBackup = mirror;
+        }
+    }
+}
