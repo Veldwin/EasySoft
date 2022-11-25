b7d48f31a4d43602234826974f1c6b352cddb32d
Veldwin <link334933@gmail.com>
2022-11-25 18:31:34 +0100

    merge

--- EasySoft/Controler/Controller.cs
+++ EasySoft/Controler/Controller.cs
@@ -19,7 +19,65 @@ namespace EasySoft.controller
             model = new Model();
             view = new View();
             view.show_start();
-            
+            model.UserMenuInput = first_menu();
+        }
+
+
+        /// <summary>
+        /// launch the console and react to the input of the user if registered
+        /// </summary>
+        /// <returns></returns>
+        private string first_menu()
+        {
+            bool menu = true;
+            while (menu)
+            {
+                model.check_data_file();
+                try
+                {
+                    view.show_menu();
+                    InputMenu = int.Parse(Console.ReadLine());
+                    switch (InputMenu)
+                    {
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+
+                        case 1:
+                            view.show_name_file();
+                            string JsonString = File.ReadAllText(model.BackupListFile);
+                            Backup[] list = JsonConvert.DeserializeObject<Backup[]>(JsonString);
+                            foreach (var Obj in list)
+                            {
+                                Console.WriteLine(" -- " + Obj.SaveName);
+                            }
+                            view.show_file();
+                            string input_name_backup = Console.ReadLine();
+                            model.load_save(input_name_backup);
+                            break;
+
+                        case 2:
+                            if (model.CheckDataBackup < 5)
+                            {
+                                Console.Clear();
+                                view.show_sub_menu();
+                                sub_menu();
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                view.error_menu("You already have 5 backups to create.");
+                            }
+                            break;
+                    }
+
+                }
+                catch
+                {
+                    Console.Clear();//Console cleaning
+                }
+            }
+            return "";
         }
     }
 }
