8974dd2ea9474a0bde89b00c1fbd5728879f5cfb
camille351 <95225227+camille351@users.noreply.github.com>
2022-11-25 14:11:24 +0100

    Classe FileCompare

---
+++ EasySoft/Model/DataState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySoft.Model
+{
+    class DataState
+    {
+        // Declaration of properties that are used for saving information for the report file in JSON
+        public string SaveNameState { get; set; }
+        public string BackupDateState { get; set; }
+        public bool SaveState { get; set; }
+        public string SourceFileState { get; set; }
+        public string TargetFileState { get; set; }
+        public float TotalFileState { get; set; }
+        public long TotalSizeState { get; set; }
+        public float ProgressState { get; set; }
+        public long FileRestState { get; set; }
+        public long TotalSizeRestState { get; set; }
+
+        public DataState(string saveNameState)
+        {
+            SaveNameState = SaveNameState;
+        }
+    }
+}
