65e399ff43080e362df7fea1cc1ea81e9f3c30a9
Veldwin <link334933@gmail.com>
2022-12-02 15:04:58 +0100

    creation Model classes

---
+++ V2/EasySave V2.0/model/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave_V2._0.model
+{
+    internal class Model
+    {
+    }
+}
