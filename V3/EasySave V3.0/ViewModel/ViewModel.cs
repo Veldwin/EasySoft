67446a895537b73d3f069684d93748512b8e531f
Mathis WAUTERS <mathis.wauters@viacesi.fr>
2022-12-02 22:45:50 +0100

    Start V3 (Duplicate V2)

---
+++ V3/EasySave V3.0/ViewModel/ViewModel.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+using EasySaveApp.model;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace EasySaveApp.viewmodel
+{
+    public class ViewModel
+    {
+        private Model model;
+        string[] jail_apps = Model.GetJailApps();
+        public string[] BlackListApp { get => jail_apps; set => jail_apps = value; }
+
+        public ViewModel()
+        {
+            model = new Model();
+        }
+
+
+        public void AddSaveModel(string type, string saveName, string sourceDir, string targetDir, string mirrorDir)//Function that allows you to add a backup
+        {
+            model.SaveName = saveName;
+            Backup backup = new Backup(model.SaveName, sourceDir, targetDir, type, mirrorDir);
+            model.AddSave(backup); // Calling the function to add a backup job
+        }
+
+
+        public List<string> ListBackup()//Function that lets you know the lists of the names of the backups.
+        {
+
+            List<string> nameslist = new List<string>();
+            foreach (var obj in model.NameList())
+            {
+                nameslist.Add(obj.SaveName);
+            }
+            return nameslist;
+        }
+
+        public bool LoadBackup(string backupname)//Function that allows you to load the backups that were selected by the user.
+        {
+            if (!Model.CheckSoftware(BlackListApp))//If a program is in the blacklist we do not start the backup.
+            {
+                model.LoadSave(backupname);//Function that launches backups
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void DeleteBackup(string backupname)//Function that allows you to delete the backups that were selected by the user.
+        {
+            model.DeleteSave(backupname);
+        }
+
+        public void ModelFormat(bool format)
+        {
+            model.ModelFormat(format);
+        }
+    }
+}
